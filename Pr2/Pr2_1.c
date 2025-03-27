dbd6dd41c06df393666792f5906764f5a1931a9b
Khristina Kud <kud.christina@lll.kpi,ua>
2025-03-27 19:18:48 +0200

    редагування

--- Pr2/Pr2_1.c
+++ Pr2/Pr2_1.c
@@ -1,27 +1,20 @@
+#include <limits.h>
 #include <stdio.h>
 #include <time.h>
 #include <limits.h>
 
-// Основна функція
 int main() {
-    // Оголошення змінної для зберігання максимального значення time_t
-    time_t max_time;
-
-    // Визначення максимального значення time_t залежно від архітектури
-#if (INT_MAX == 2147483647) // 32-бітний time_t
-    max_time = (time_t)INT_MAX;
-#else // 64-бітний time_t
-    max_time = (time_t)LLONG_MAX;
-#endif
-
-    // Конвертація максимального значення time_t у структуру часу
-    struct tm *tm_info = localtime(&max_time);
-    if (tm_info) { // Якщо конвертація успішна
-        printf("Maximum representable time: %s", asctime(tm_info)); // Виведення дати
-    }
-    else { // Якщо конвертація не вдалася
-        printf("Time conversion failed, value might be too large\n");
+    // Установлюємо максимальне значення для time_t
+    time_t max_time =LONG_MAX; // LONG_MAX підходить для time_t як ціле число
+    //  макс значення
+    printf("Max value time_t: %lld\n", (long long)max_time);
+    // Відображення часу у форматі
+    struct tm *max_time_tm = gmtime(&max_time);
+    if (max_time_tm) {
+        printf("Time? when time_t finished: %s", asctime(max_time_tm));
+    } else {
+        printf("Error.\n");
     }
 
-    return 0; // Повернення нуля для успішного завершення програми
+    return 0;
 }
