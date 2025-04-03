9bb3edb338a300d7a947e1a48febb6a05b29576a
Khristina Kud <kud.christina@lll.kpi,ua>
2025-04-03 15:36:45 +0300

    редагування

--- Pr2/Pr22.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int global_array[1000] = {1};
-int main() {
-    int local_array[1000];          // Неініціалізований локальний масив
-    int local_array_init[1000] = {1}; // Ініціалізований локальний масив
-    printf("Hello, World!\n");
-    return 0;
-}
