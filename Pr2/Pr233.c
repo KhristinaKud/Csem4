a7ccda6cbee6901e585884d13f7f50537afff832
Khristina Kud <kud.christina@lll.kpi,ua>
2025-04-03 18:21:54 +0300

    редагування

--- Pr2/Pr233.c
+++ Pr2/Pr233.c
@@ -3,13 +3,14 @@
 void increase_stack() {
     int array1[1000];
     int array2[1000];
-    int array3[1000];
-    printf("Stack top inside function (array1): %p\n", (void*)&array1[0]);
-    printf("Stack top inside function (array2): %p\n", (void*)&array2[0]);
-    printf("Stack top inside function (array3): %p\n", (void*)&array3[0]);
+    printf("Stack top inside function (array1): %p\n", &array1);
+    printf("Stack top inside function (array2): %p\n", &array2);
+
 }
 
 int main() {
+    int a;
+    printf("Initial stack top: %p\n", &a);
     increase_stack();
     return 0;
 }
