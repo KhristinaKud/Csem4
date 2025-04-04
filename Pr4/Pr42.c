be0581f6e86f0972c3f8b5655d101c62e70278bd
Khristina Kud <kud.christina@lll.kpi,ua>
2025-04-04 22:52:04 +0300

    add

--- Pr4/Pr42.c
+++ Pr4/Pr42.c
@@ -1,3 +1,29 @@
-//
-// Created by User01 on 04.04.2025.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int main() {
+    int a = -1;
+    void *ptr = malloc(a);
+    if (ptr == NULL) {
+        printf("malloc cannot allocate memory for a negative size\n");
+    } else {
+        printf("malloc can allocate memory for negative size\n");
+        free(ptr);
+    }
+
+    int xa = INT_MAX;
+    printf("xa = %d\n", xa);
+    int xb = 2;
+    int num = xa * xb;
+    printf("num = %d\n", num);
+    void *ptr2 = malloc(num);
+    if (ptr == NULL) {
+        printf("malloc failed\n");
+    } else {
+        printf("Allocated %d bytes\n", num);
+        free(ptr);
+    }
+
+    return 0;
+}
