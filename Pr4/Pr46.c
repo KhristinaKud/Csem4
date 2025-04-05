4f6a9fd9f97bc5bc539e7563ac254b9916e776a2
Khristina Kud <kud.christina@lll.kpi,ua>
2025-04-05 12:57:35 +0300

    add

--- Pr4/Pr46.c
+++ Pr4/Pr46.c
@@ -3,7 +3,7 @@
 
 int main() {
     void *ptr1 = realloc(NULL, 6); //  malloc(6)
-    printf("ptr 1return: %p\n", ptr1);
+    printf("ptr1 return: %p\n", ptr1);
     void *ptr2 = realloc(ptr1, 0);   //  free(ptr1)
     printf("ptr2 return: %p\n", ptr2);
     return 0;
