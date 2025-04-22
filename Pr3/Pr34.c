5204e267a9898fa4ce9f95997ebe6e046b7c40f2
Khristina Kud <kud.christina@lll.kpi,ua>
2025-04-22 23:14:15 +0300

    add

--- Pr3/Pr34.c
+++ Pr3/Pr34.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #define MAX_CPU_TIME 2
 
-// Функція для генерації унікальних чисел у діапазоні
+
 void generate_numbers(int *num, int count, int max_range) {
     for (int i = 0; i < count; i++) {
         num[i] = rand() % max_range + 1;
@@ -16,7 +16,7 @@ void generate_numbers(int *num, int count, int max_range) {
     }
 }
 
-// Функція для друку масиву чисел
+
 void print_numbers(const char *label, int *num, int count) {
     printf("%s: ", label);
     for (int i = 0; i < count; i++) {
@@ -30,7 +30,7 @@ int main() {
     time_t start_time, current_time;
     double elapsed_time;
 
-    srand(time(NULL));  // Ініціалізація генератора випадкових чисел
+    srand(time(NULL));
 
     start_time = time(NULL);
 
@@ -45,8 +45,8 @@ int main() {
         }
 
         // Генерація чисел для обох лотерей
-        generate_numbers(lottery1, 7, 49);  // 7 чисел із діапазону 1-49
-        generate_numbers(lottery2, 6, 36);  // 6 чисел із діапазону 1-36
+        generate_numbers(lottery1, 7, 49);
+        generate_numbers(lottery2, 6, 36);
 
         // Виведення результатів
         print_numbers("Lottery numbers (1-49)", lottery1, 7);
--- Pr3/Pr37.c
+++ Pr3/Pr37.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 
 int main() {
-    size_t block_size = 1024 * 1024; // 1 MB
+    size_t size = 1024;
     int count = 0;
     while (1) {
-        char* ptr = malloc(block_size);
+        char* ptr = malloc(size);
         if (ptr == NULL) {
             printf("Memory allocation failed at block %d (approx %lu MB allocated)\n", count, count);
             break;
         }
         count++;
-        printf("Allocated %d MB\n", count);
     }
     return 0;
 }
