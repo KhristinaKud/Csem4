4768ef6d291180e8eac87be80dfbb45f3222bf94
Khristina Kud <kud.christina@lll.kpi,ua>
2025-05-26 23:52:36 +0300

    add

--- Pr8/Pr81.c
+++ Pr8/Pr81.c
@@ -1,11 +1,38 @@
-#include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
 
 int main() {
-    int fd = open("test.txt", O_WRONLY | O_CREAT, 0644);
-    char buffer[] = "Hello";
-    ssize_t count = write(fd, buffer, 5);
-    printf("Written %zd bytes out of 5\n", count);
+    int fd;
+    char *buffer;
+    size_t nbytes = 100000;
+    ssize_t count;
+    fd = open("test.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        perror("open");
+        exit(EXIT_FAILURE);
+    }
+
+    buffer = malloc(nbytes);
+    if (buffer == NULL) {
+        perror("malloc");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    memset(buffer, 'A', nbytes);
+    count = write(fd, buffer, nbytes);
+    if (count == -1) {
+        perror("write");
+        free(buffer);
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Requested to write: %zu bytes\n", nbytes);
+    printf("Actually written: %zd bytes\n", count);
+
+    free(buffer);
     close(fd);
     return 0;
 }
