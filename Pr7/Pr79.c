cdf5bfe92099823ba4740948eb0a3457b868d8b4
Khristina Kud <kud.christina@lll.kpi,ua>
2025-05-26 16:04:55 +0300

    add

--- Pr7/Pr79.c
+++ Pr7/Pr79.c
@@ -9,20 +9,20 @@ int main() {
     char target_ip[MAX_IP_LEN];
     int start_port, end_port;
 
-    printf("Введіть IP-адресу для сканування: ");
+    printf("Enter the IP address to scan: ");
     scanf("%15s", target_ip);
 
-    printf("Введіть початковий порт (1-%d): ", MAX_PORTS);
+    printf("Enter the starting port (1-%d): ", MAX_PORTS);
     scanf("%d", &start_port);
-    printf("Введіть кінцевий порт (1-%d): ", MAX_PORTS);
+    printf("Enter the final port (1-%d): ", MAX_PORTS);
     scanf("%d", &end_port);
 
     if (start_port < 1 || end_port > MAX_PORTS || start_port > end_port) {
-        printf("Некоректний діапазон портів!\n");
+        printf("Incorrect port range!\n");
         return 1;
     }
 
-    printf("Сканування портів %d-%d на хості %s...\n", start_port, end_port, target_ip);
+    printf("Scan ports %d-%d on host %s\n", start_port, end_port, target_ip);
 
     char command[256];
     for (int port = start_port; port <= end_port; port++) {
